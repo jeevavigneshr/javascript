e3cdd9d785631c03a518479968dcbaac15eb6472
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-07-06 19:19:43 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,11 @@
+ var actor = "vijay"
+ var player = "dhoni"
+ var movie = "leo"
+ 
+ function favourite()
+ {
+    console.log("Favourite actor:" + actor);
+    console.log("favourite player:"+ player);
+    console.log("favourite movie:"+ movie);
+ }
+ favourite()
